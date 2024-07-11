e2c56609fbb5b1a7b4399a42a47469c900b43d86
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2024-07-11 23:17:07 +0200

    remove 2004 and 2008 years;

--- src/preprocessing/groupByYears.ts
+++ src/preprocessing/groupByYears.ts
@@ -4,7 +4,11 @@ import { AccidentData } from "../interfaces/AccidentData";
 function groupByYears(
   data: Array<AccidentData>,
 ): Map<number, Array<AccidentData>> {
-  return group(data, (d) => d.date.getUTCFullYear());
+  const res = group(data, (d) => d.date.getUTCFullYear());
+  // these two years have a lot of invalid data (e.g., date, road type, etc.)
+  res.delete(2004);
+  res.delete(2008);
+  return res;
 }
 
 export { groupByYears };
